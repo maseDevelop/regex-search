4bc1150eefb1d284d031b5243bbeceb269609671
Jonecon <connor.gauthern@gmail.com>
2020-05-04 21:55:27 +1200

    Commented and cleaned

--- REsearch.java
+++ REsearch.java
@@ -5,17 +5,16 @@ import java.io.*;
 
 public class REsearch {
 
+	//Global arrays
 	private ArrayList<String> characterArray = new ArrayList<String>();
     	private ArrayList<Integer> nextStateOne = new ArrayList<Integer>();
     	private ArrayList<Integer> nextStateTwo = new ArrayList<Integer>();
     	private ArrayList<Boolean> visited = new ArrayList<Boolean>();
     	
-    	boolean found;
-    	
-    	
-    	REdeque deque;
-    	
-    	public int m = 0, p = 0;
+    	//Global variables 
+    	private boolean found;
+    	private REdeque deque;
+    	private int m = 0, p = 0;
 
 	public static void main(String[] args) {
 		if (args.length != 1){
@@ -29,16 +28,16 @@ public class REsearch {
 	
 	public void run(String[] args){
 		try{
+			//Variable init
 			deque = new REdeque();
-		
 			BufferedReader tableIn = new BufferedReader(new InputStreamReader(System.in));
 			FileReader file = new FileReader(new File(args[0]));
 			BufferedReader fileIn = new BufferedReader(file);
-			
 			String line;
 			String[] inputArr;
 			int s1, s2, state;
 		
+			//Reads the table and sets it up for our search.
 			while ((line = tableIn.readLine()) != null){
 			inputArr = line.split(",");
 			characterArray.add(inputArr[1]);
@@ -48,9 +47,10 @@ public class REsearch {
 			}
 			tableIn.close();
 			
-			
+			//Reads every line of the file
 			while ((line = fileIn.readLine()) != null){
 				found = false;
+				//Reads every character in that line
 				for (int i = 0; i < line.length(); i++){
 					m = i;
 					p = i;
@@ -63,20 +63,21 @@ public class REsearch {
 					//Reset visited to false
 					resetVisited();
 					
+					//Proccesses every state in the deque
 					while (true){
 						//No characters to match states left. Move m forward.
-						if (deque.isEmpty()){
+						if (deque.isEmpty())
 							break;
-						}
 						
 						//Get top of current states stack.
 						state = deque.removeFront();
 						
-						//Work the state
+						//Proccesses the current state
 						if (checkState(state, line))
 							break;
 						
 					}
+					//Stops loop so we read next line once we have found a match in this line.
 					if (found)
 						break;
 				}
@@ -89,11 +90,13 @@ public class REsearch {
 		}
 	}
 	
+	//Resets visited back to false for every index.
 	public void resetVisited(){
 		for(int i = 0; i < visited.size(); i++)
 			visited.set(i, false);
 	}
 	
+	//Takes the state, identifies it, and then proccesses it based on what it is.
 	public boolean checkState(int state, String line){
 	
 		if(state == -2){
@@ -126,22 +129,17 @@ public class REsearch {
 			return true;
 			
 		}else{
-			if (characterArray.get(state).compareTo("wildcard") == 0){
-				//comment
-				deque.addRear(nextStateOne.get(state));							
-			}else{
-				//Comment		
-				if (!(p >= line.length())){
+			if (!(p >= line.length()))
+				if (characterArray.get(state).compareTo("wildcard") == 0)
+					deque.addRear(nextStateOne.get(state));	
+				else{
 					//Check the character, if same increase pointer
-					char c = line.charAt(p);
-					char e = characterArray.get(state).charAt(0);
-					if (c == e){
+					if (line.charAt(p) == characterArray.get(state).charAt(0)){
 						//Add these onto stack
 						deque.addRear(nextStateOne.get(state));
 						visited.set(state, true);	
 					}
-				}	
-			}				
+				}				
 		}
 		return false;
 	}
